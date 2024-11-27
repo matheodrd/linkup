5f8e98786b5946aa1609b0398bfedc8055c28d34
matheodrd <matheodurand14@gmail.com>
2024-11-28 00:42:43 +0100

    add lifespan function - create db/tables on startup

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
 from typing import Dict
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 import uvicorn
 
 from database_setup import create_db_and_tables
 
-app = FastAPI(title="Link Up")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(title="Link Up", lifespan=lifespan)
 
 @app.get("/")
 async def index() -> Dict[str, str]:
     return {"message": "hello amigo :-)"}
 
 if __name__ == "__main__":
-    create_db_and_tables()
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
