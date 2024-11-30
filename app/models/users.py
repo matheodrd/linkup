e192287433f72abd99db1cf188f9feb75e5a8342
matheodrd <matheodurand14@gmail.com>
2024-11-30 23:46:27 +0100

    add Profile models and relationship with User

--- app/models/users.py
+++ app/models/users.py
@@ -1,6 +1,14 @@
+"""
+This file contains models for the `User` and `Profile` tables.
+They are in the same file to avoid circular imports. They are very tightly related.
+"""
+from __future__ import annotations
 import uuid
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import SQLModel, Field, Relationship
+
+################
+##### USER #####
 
 class UserBase(SQLModel):
     email: str = Field(index=True, unique=True)
@@ -8,6 +16,8 @@ class UserBase(SQLModel):
 
 class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    # profile will be None for an instant before the creation of the profile
+    profile: Profile | None = Relationship(back_populates="user")
 
 class UserCreate(UserBase):
     pass
@@ -18,3 +28,25 @@ class UserUpdate(SQLModel):
 
 class UserPublic(UserBase):
     id: uuid.UUID
+
+###################
+##### PROFILE #####
+
+class ProfileBase(SQLModel):
+    bio: str = Field(default="Hello!, I'm new here")
+    profile_picture: str | None = None
+    is_private: bool = Field(default=False)
+
+class Profile(ProfileBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    user_id: uuid.UUID = Field(foreign_key="user.id", nullable=False)
+    user: User = Relationship(back_populates="profile")
+
+class ProfileUpdate(SQLModel):
+    bio: str | None = None
+    profile_picture: str | None = None
+    is_private: bool | None = None
+
+class ProfilePublic(ProfileBase):
+    id: uuid.UUID
+    user_id: uuid.UUID
