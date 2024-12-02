72eb7a3b65109d050ca3ce303ba06cb7a574f5f8
matheodrd <matheodurand14@gmail.com>
2024-12-03 00:12:26 +0100

    add media_dir envvar in config

--- app/config.py
+++ app/config.py
@@ -5,5 +5,8 @@ class Settings(BaseSettings):
     DATABASE_URL: str = os.getenv(
         "DATABASE_URL", "sqlite:///./dev.db"
     )
+    MEDIA_DIR: str = os.getenv(
+        "MEDIA_DIR", "static/media"
+    )
 
 settings = Settings()
