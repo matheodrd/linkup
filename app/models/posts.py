1e9528f6d344df80e7118865eba8fa97ffb7b281
matheodrd <matheodurand14@gmail.com>
2024-12-02 10:12:42 +0100

    add max length for Post content

--- app/models/posts.py
+++ app/models/posts.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from sqlmodel import SQLModel, Field
 
 class PostBase(SQLModel):
-    content: str
+    content: str = Field(max_length=2200)
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime | None = None
 
@@ -13,11 +13,11 @@ class Post(PostBase, table=True):
     user_id: uuid.UUID = Field(foreign_key="user.id", nullable=False)
 
 class PostCreate(SQLModel):
-    content: str
+    content: str = Field(max_length=2200)
     user_id: uuid.UUID
 
 class PostUpdate(SQLModel):
-    content: str | None = None
+    content: str | None = Field(default=None, max_length=2200)
 
 class PostPublic(PostBase):
     id: uuid.UUID
